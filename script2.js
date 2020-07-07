068fa249edfe37b7640035c80facfd8497f25124
David Harbin <david_k_harbin@hotmail.com>
2020-07-06 18:13:24 -0600

    prototypes

--- script2.js
+++ script2.js
@@ -1,40 +1,26 @@
-class User {
-  constructor(email, name){
-    this.email = email;
-    this.name = name;
-    this.score = 0;
-  }
-  login(){
-    console.log(`${this.email}, just logged in`);
-    return this;
-  }
-  logout(){
-    console.log(`${this.email}, just logged out`);
-    return this;
-  }
-  updateScore(){
-    this.score++;
-    console.log(`${this.email}, 'score is now', ${this.score}`);
-    return this;
-  }
+function User(email, name){
+  this.email = email;
+  this.name = name;
+  this.online = false;
 }
 
-
-class Admin extends User {
-  deleteUser(user){
-    users = users.filter(u => {
-      return user.email != user.email;
-    })
-  }
+User.prototype.login = function(){
+  this.online = true;
+  console.log(`${this.email}, has logged in.`);
 }
 
-let userOne = new User('ryu@ninjas.com', 'Ryu');
-let userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
-let admin = new Admin('sean@ninjas.com', 'Sean');
+User.prototype.logout = function(){
+  this.online = false;
+  console.log(`${this.email}, has logged out.`);
+}
 
+function Admin(...args){
+  User.apply(this, args)
+  this.role = 'super admin';
+}
 
-let users = [userOne, userTwo, admin];
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+let admin = new Admin('sean@ninjas.com', 'sean');
 
-// admin.deleteUser(userOne);
-
-console.log(users);
+console.log(admin);
