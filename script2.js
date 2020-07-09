a3fbe4ea47b674db1057553888b482ab116409ab
davidkharbin <david_k_harbin@hotmail.com>
2020-07-09 02:57:44 -0600

    overwrite me

--- script2.js
+++ script2.js
@@ -1,40 +1,18 @@
-class User {
-  constructor(email, name){
-    this.email = email;
-    this.name = name;
-    this.score = 0;
-  }
-  login(){
-    console.log(`${this.email}, just logged in`);
-    return this;
-  }
-  logout(){
-    console.log(`${this.email}, just logged out`);
-    return this;
-  }
-  updateScore(){
-    this.score++;
-    console.log(`${this.email}, 'score is now', ${this.score}`);
-    return this;
+function User(email, name){
+  this.email = email;
+  this.name = name;
+  this.online = false;
+  this.login = function(){
+    console.log(`${this.email}, has logged in`);
   }
 }
 
 
-class Admin extends User {
-  deleteUser(user){
-    users = users.filter(u => {
-      return user.email != user.email;
-    })
-  }
-}
+
+
 
 let userOne = new User('ryu@ninjas.com', 'Ryu');
 let userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
-let admin = new Admin('sean@ninjas.com', 'Sean');
 
-
-let users = [userOne, userTwo, admin];
-
-// admin.deleteUser(userOne);
-
-console.log(users);
+console.log(userOne);
+userTwo.login();
