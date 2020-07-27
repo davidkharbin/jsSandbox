33e5bd3896ca99ae591345b0f43a878510161d51
David Harbin <david_k_harbin@hotmail.com>
2020-07-27 05:55:11 -0600

    hackreactor practice

--- script2.js
+++ script2.js
@@ -16,3 +16,16 @@ const john = new Person('John', 'Doe', '8/12/90');
 const mary = new Person('Mary', 'Johnson', 'March 20 1978');
 
 console.log(mary);
+
+
+function indexOfString(string1, string2) {
+  
+  arr1 = Array.from(string1);
+  arr2 = Array.from(string2);
+  
+  for (let i = 0; i < arr1.length || arr2.length; i++){
+      if (arr1[i] == arr2[i]) {
+          return i;
+      }
+  }
+}
