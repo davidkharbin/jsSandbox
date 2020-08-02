99e5579e5dd8e3b3b002da3f67b2720d005015d8
davidkharbin <david_k_harbin@hotmail.com>
2020-08-01 22:21:23 -0600

    prep course

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-var fruits = ['oranges', 'bananas', 'apples'];
-var scores = [98, 85, 91, 78, 82];
-var empty = [];
+var resultBoolean1 = isAnObject([1, 2, 3]);
+console.log('should log false:', resultBoolean1);
 
-console.log('fruits:', fruits);
-console.log('scores:', scores);
-console.log('empty:', empty);
+var resultBoolean2 = isAnObject({name: 'Tom', friend: 'Jerry'});
+console.log('should log true:', resultBoolean2);
 
-
-let tempVar = 'oranges';
-
-console.log(tempVar.indexOf('r'));
-
-
+function isAnObject(input) {
+    
+    isObject = tyepof input === "object";
+    isNotAnArray = (!Array.isArray(object));
+    
+    return isObjectAndNotAnArray = (isObject && isNotAnArray);
+    
+  // create an isObject variable
+  // assign it to whether the input is an object type
+  // create an isNotAnArray variable
+  // assign it to whether the input is not an array
+  // create an isObjectAndNotAnArray variable
+  // assign it to a combination of isObject AND isNotAnArray
+  // return the isObjectAndNotAnArray variable
+};
