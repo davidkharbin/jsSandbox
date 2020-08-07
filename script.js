8237988537324d058da423136cf8269f352c4a8a
davidkharbin <david_k_harbin@hotmail.com>
2020-08-07 05:11:55 -0600

    functions

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-generateCombinations(['a', 'b', 'c'], ['d', 'e', 'f']);
-// console output:
-  // a d
-  // a e
-  // a f
-  // b d
-  // b e
-  // b f
-  // c d
-  // c e
-  // c f
 
-generateCombinations([1, 2], ['buckle', 'my', 'shoe']);
-// console output:
-  // 1 buckle
-  // 1 my
-  // 1 shoe
-  // 2 buckle
-  // 2 my
-  // 2 shoe
+
+var result1 = createSentence(['I', 'am', 'worth', 'it']);
+console.log('should log "I am worth it.":', result1);
+
+var result2 = createSentence(['My', 'problems', 'matter']);
+console.log('should log "My problems matter.":', result2);
 
 
 
-function generateCombinations(array1, array2) {
+
+
+function createSentence(words) {
+  
+  var sentence = "";
+
+  for (var i = 0; i < words.length; i++) {
+    // hint: does this line need to happen every iteration?    
+    if(i < words.length - 1){
+      sentence += words[i] + ' ';
+    }
     
-  for(let i = 0; i < array1.length; i++){
-      for(let j = 0; j < array2.length; j++){
-          console.log(`${array1[i]} ${array2[j]}`)
-      }
+
+  if (i === words.length - 1){
+    sentence += words[i] +'.';
   }
-// create a loop which iterates over the first array
-  // create an inner loop which iterates over the second array
-    // log current element of first array and current element of second array to the console with space in between
+        
+  }
+    return sentence;
 }
+
+
+  
