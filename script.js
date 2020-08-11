b04da68323d0a406a8e82f72d5711b517075037c
davidkharbin <david_k_harbin@hotmail.com>
2020-08-11 11:59:00 -0600

    merge

--- script.js
+++ script.js
@@ -1,60 +1,11 @@
-function countAllCharacters(string) {
-  if(string === '') {
-      return {};
-  }
-  
-  let characterObj = {};
-  let characterArray = string.split('');
+var resultKeys = getAllKeysAgain({firstScore: {part1: 12, part2: 14, part3: 10}, secondScore: 34, thirdScore: 28}, 'firstScore');
+console.log('should log ["par1", "part2", "part3"]:', resultKeys);
 
-  for(let i = 0; i < characterArray.length; i++) {
-    let currentChar = characterArray[i];
 
-    if(characterObj[currentChar] === undefined) {
-      characterObj[currentChar] = 1;
-    } else {
-      characterObj[currentChar]++;
-    }
-  }
-  return characterObj;
+function getAllKeysAgain(obj, key) {
+  let keys = Object.keys(obj[key]);
+  return keys
+// create a keys variable
+// assign it to an expression which will generate an array of all of the keys located within obj at key
+// return to the keys variable
 }
-
-
-var result1 = countAllCharacters('banana');
-console.log('should log "{b: 1, a: 3, n: 2}":', result1);
-
-var result2 = countAllCharacters('');
-console.log('should log "{}":', result2); 
-
-
-
-var result1 = createSentence(['I', 'am', 'worth', 'it']);
-console.log('should log "I am worth it.":', result1);
-
-var result2 = createSentence(['My', 'problems', 'matter']);
-console.log('should log "My problems matter.":', result2);
-
-
-
-
-
-function createSentence(words) {
-  
-  var sentence = "";
-
-  for (var i = 0; i < words.length; i++) {
-    // hint: does this line need to happen every iteration?    
-    if(i < words.length - 1){
-      sentence += words[i] + ' ';
-    }
-    
-
-  if (i === words.length - 1){
-    sentence += words[i] +'.';
-  }
-        
-  }
-    return sentence;
-}
-
-
-  
