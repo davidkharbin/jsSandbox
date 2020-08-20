91e1d5a0a01fc1b9d1282689c2ecbd495aa7b793
David Harbin <david_k_harbin@hotmail.com>
2020-08-20 15:54:54 -0600

    Math 5 On branch master

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-var obj = {
-  key: [1, 2, 6]
-};
-var output = getNthElementOfProperty(obj, 'key', 1);
-console.log(output); // --> 2
+let arr = [1, 2, 3, 4, 5];
 
-
-
-function getNthElementOfProperty(obj, key, n) {
-  if ((obj[key] === undefined) || (obj[key].length === 0 || n > obj[key].length) || (!Array.isArray(obj[key]))){
-    return undefined
-  }
-
-  return obj[key][1]
-}
-  key: [1, 2, 3]
-};
-var output = getAverageOfElementsAtProperty(obj, 'key');
-console.log(output); // --> 2
-
-
-function getAverageOfElementsAtProperty(obj, key) {
-    
-  if ((obj.key === undefined) || (obj.key.length === 0) || (!Array.isArray(obj.key))){
-      return 0
-  }
-  
-  let sum = 0;
-  
-  for (let i = 0; i < obj[key].length; i++){
-      sum += obj[key][i];
-  }
-  
-  return sum / obj[key].length;
-}
-
+console.log(arr[-1]);
