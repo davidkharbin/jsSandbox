ea59b0882c5ddf1f1480db3070b2f55c60bf2d9d
David Harbin <david_k_harbin@hotmail.com>
2020-08-20 20:17:57 -0600

    array methoods 15

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-let arr = [1, 2, 3, 4, 5];
+var output = findShortestWordAmongMixedElements([4, 'two', 2, 'up', 'three']);
+console.log(output); // --> 'two'
 
-console.log(arr[-1]);
+
+function findShortestWordAmongMixedElements(arr) {
+  let stringArray = [];
+  
+  for (let i = 0; i < arr.length; i++){
+      if (typeof arr[i] === 'string'){
+          stringArray.push(arr[i]);
+      }
+  }
+
+  if (stringArray.length === 0 || arr.length === 0){
+    return ''
+  }
+
+  let shortest = stringArray[0];
+  for (let j = 0; j < stringArray.length; j++){
+    if (stringArray[j].length < shortest.length)
+    shortest = stringArray[j];
+  }
+
+  
+
+  return shortest
+}
