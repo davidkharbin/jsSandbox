dda5e39fa4a3386158d583cacaec5bdd8e442b4e
davidkharbin <david_k_harbin@hotmail.com>
2020-08-21 00:04:12 -0600

    stash

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-var obj = {
-  key: [1, 2, 6]
-};
-var output = getNthElementOfProperty(obj, 'key', 1);
-console.log(output); // --> 2
+// Write a function called "findShortestWordAmongMixedElements".
+
+// Given an array, "findShortestWordAmongMixedElements" returns the shortest string within the given array.
+
+// Notes:
+
+//     If there are ties, it should return the first element to appear in the given array.
+//     Expect the given array to have values other than strings.
+//     If the given array is empty, it should return an empty string.
+//     If the given array contains no strings, it should return an empty string.
 
 
 
-function getNthElementOfProperty(obj, key, n) {
-  if ((obj[key] === undefined) || (obj[key].length === 0 || n > obj[key].length) || (!Array.isArray(obj[key]))){
-    return undefined
+
+var output = findShortestWordAmongMixedElements([4, 'two', 2, 'three']);
+console.log(output); // --> 'two'
+
+
+
+function findShortestWordAmongMixedElements(arr) {
+  let shortest = Infinity;
+  
+  for (let i =0; i < arr.length; i++){
+      if (arr[i].length < shortest){
+          shortest = arr[i];
+      }
   }
-
-  return obj[key][1]
+  
+  return shortest
 }
-  key: [1, 2, 3]
-};
-var output = getAverageOfElementsAtProperty(obj, 'key');
-console.log(output); // --> 2
-
-
-function getAverageOfElementsAtProperty(obj, key) {
-    
-  if ((obj.key === undefined) || (obj.key.length === 0) || (!Array.isArray(obj.key))){
-      return 0
-  }
-  
-  let sum = 0;
-  
-  for (let i = 0; i < obj[key].length; i++){
-      sum += obj[key][i];
-  }
-  
-  return sum / obj[key].length;
-}
-
