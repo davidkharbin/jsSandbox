76803e2ddf4c5718c0bcd1dbbe84ab1da5f711cb
David Harbin <david_k_harbin@hotmail.com>
2020-08-27 02:53:39 -0600

    testing

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
+var expected = ['b', 'r', 'o', 'k', 'e', 'n'];
+var actual = 'broken'.split('');
+assertArraysEqual(actual, expected, 'splits string into array of characters');
+// console output:
+// passed
 
-var customerData = {
-  'Joe': {
-    visits: 1
-  },
-  'Carol': {
-    visits: 2
-  },
-  'Howard': {
-    visits: 3,
-  },
-  'Carrie': {
-    visits: 4
+
+var expected = [1, 1, 2, 3, 5, 8, 13];
+var actual = generateFirstNFibonaccis(7);
+assertArraysEqual(actual, expected, 'generates first 7 Fibonacci numbers');
+// console output:
+// FAILED [generates first 7 Fibonacci numbers] Expected "1, 1, 2, 3, 5, 8, 13", but got "2, 3, 5, 8, 13, 21, 34"
+
+
+
+
+function assertArraysEqual(actual, expected, testName) {
+  let equalLength = (actual.length === expected.length);
+  let equalValues = true;
+      
+  for (let i = 0; i < actual.length; i++){
+    if (actual[i] !== expected[i]){
+      equalValues = false;
+      break;
+    }
   }
-};
 
-var output = greetCustomer('Carrie');
-console.log(output);
-
-
-function greetCustomer(firstName) {
-  if (customerData[firstName] === undefined){
-      return 'Welcome! Is this your first time?';
-  }
-  
-  // var visitsObject = customerData[firstName];
-  if (customerData[firstName]['visits'] === 1){
-      return "Welcome back, " + firstName + "! We're glad you liked us the first time!";
+  if (equalLength && equalValues){
+    console.log('passed');
   } else {
-      return "Welcome back, " + firstName + "! So glad to see you again!";
+    console.log(`FAILED [${testName}] Expected "${expected}", but got "${actual}"`);
   }
 }
