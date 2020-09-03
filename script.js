be094a11cddc767d27f44f22e8a21d96f17e5c9d
davidkharbin <david_k_harbin@hotmail.com>
2020-09-03 05:50:31 -0600

    TAA prep

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-var input = {
-  name: 'Holly',
-  age: 35,
-  role: 'producer'
+// FUNCTION DEFINITION(S)
+function square(n) {
+  return n * n;
 }
 
-
-var output = [['name', 'Holly'], ['age', 35], ['role', 'producer']];
-
-console.log(convertObjectToArray(input));
-
-function convertObjectToArray(obj) {
-
-  let outputArray = [];
-  let keysArray = Object.keys(obj);
-  let valsArray = Object.values(obj);
-
-  for (i = 0; i < keysArray.length; i++){
-    outputArray[i] = [keysArray[i], valsArray[i]];
-  }
-
-  return outputArray;
-  
+// ASSERTION FUNCTION(S) TO BE USED
+function assertEqual(actual, expected, testName) {
+    if (actual === expected) {
+        console.log(`Test Name: [${testName}] PASSED!`);
+    } else {
+        console.log(`Failed: Expected ${expected} but got ${actual}`);
+    }
 }
+
+// TESTS CASES
+
+// positive
+let actual_1 = square(3);
+let expected_1 = 9;
+assertEqual(actual_1, expected_1, 'Should square a positive integer.');
+
+// negative
+let actual_2 = square(-4);
+let expected_2 = 16;
+assertEqual(actual_2, expected_2, 'Should square a negative integer.');
+
+// zero
+let actual_3 = square(0);
+let expected_3 = 0;
+assertEqual(actual_3, expected_3, 'Should square zero.');
+
+// decimal
+let actual_4 = square(0.5);
+let expected_4 = 0.25;
+assertEqual(actual_4, expected_4, 'Should square a decimal number.');
