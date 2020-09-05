d589685f3dc5ed1441dd9a1dfd5a5a16f4450c9d
davidkharbin <david_k_harbin@hotmail.com>
2020-09-05 06:05:58 -0600

    applying assertEqualsArray

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
-// Note: This is a simple, albeit temporarily incorrect implementation of the standard Array method "every()":
-// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/every
-
 // FUNCTION DEFINITION(S)
-function every(array, callbackFunction) {
-    var doesEveryElementMatch = true;
+function map(array, callbackFunction) {
+  var newArray = [];
   
-    for (var i = 0; i < array.length; i++) {
-      
-      if (doesEveryElementMatch === false) {
-          return doesEveryElementMatch;
-      }
-      doesEveryElementMatch = callbackFunction(array[i]);
+  for (var i = 0; i < array.length; i++) {
+    newArray.push(callbackFunction(array[i]))
+  }
+
+  console.log(newArray);
+  
+  return newArray;
+}
+
+function cubeAll(numbers) {
+  return map(numbers, function(n) {
+    return n**3;
+  });
+}
+  
+// ASSERTION FUNCTION(S) TO BE USED
+function assertArraysEqual(actual,expected, testName) {
+  let sameValues = true;
+  
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]){
+        sameValues = false;
+        break;
     }
-  
-    return doesEveryElementMatch;
   }
+ 
+  let sameLength = (actual.length === expected.length);
   
-  // ASSERTION FUNCTION(S) TO BE USED
-  function assertEqual(actual, expected, testName) {
-      if (actual === expected) {
-          console.log('passed');
-      } else {
-          console.log(`FAILED [${testName}]: Expected ${expected} but got ${actual}`);
-      }
+  if (sameValues && sameLength) {
+      console.log('passed');    
+  } else {
+      console.log(`Failed ${testName}`);
   }
+}
   
-  // TESTS CASES
-  function lessThan10(val) {
-      return val < 10;
-  }
+// TESTS CASES
+function addOne(val) {
+  return val + 1;
+}
 
-  let arrayTrue = [1, 3, 9, 2, 7];
-  let actualTrue = every(arrayTrue, lessThan10);
-  assertEqual(actualTrue, true, 'should return true when all values are true');
+let mapInput = [1, 2, 3, 4, 5];
+let actualMap = map(mapInput, addOne);
+let expectedMap = [2, 3, 4, 5, 6];
+assertArraysEqual(actualMap, expectedMap, 'should correctly map elements of an array');
 
-  let arrayFalse = [1, 1, 9, 2, 17];
-  let actualFalse = every(arrayFalse, lessThan10);
-  assertEqual(actualFalse, false, 'should return false if any array value is false');
+let cubeInput = [1, 2, 3, 4, 5];
+let actualCube = cubeAll(cubeInput);
+let exptectedCube = [1, 8, 27, 64, 125];
+assertArraysEqual(actualCube, exptectedCube, 'should correctly cube each element of an array');
