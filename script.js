e40c79f39beaf73ad39b8818d840df417f9833a4
davidkharbin <david_k_harbin@hotmail.com>
2020-09-07 03:41:32 -0600

    finished testing module

--- script.js
+++ script.js
@@ -1,53 +1,39 @@
 // FUNCTION DEFINITION(S)
-function map(array, callbackFunction) {
-  var newArray = [];
-  
-  for (var i = 0; i < array.length; i++) {
-    newArray.push(callbackFunction(array[i]))
+function addFullNameProp(obj) {
+  let firstName = obj['firstName'];
+  let lastName = obj['lastName'];
+  let fullName = `${firstName} ${lastName}`;
+  if (firstName && lastName) {
+    obj['fullName'] = fullName;
   }
-
-  console.log(newArray);
   
-  return newArray;
+  return obj;
 }
 
-function cubeAll(numbers) {
-  return map(numbers, function(n) {
-    return n**3;
-  });
-}
-  
 // ASSERTION FUNCTION(S) TO BE USED
-function assertArraysEqual(actual,expected, testName) {
-  let sameValues = true;
-  
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]){
-        sameValues = false;
-        break;
-    }
-  }
- 
-  let sameLength = (actual.length === expected.length);
-  
-  if (sameValues && sameLength) {
-      console.log('passed');    
+function assertObjectsEqual(actual, expected, testName) {
+  actual = JSON.stringify(actual);
+  expected = JSON.stringify(expected);
+
+  if (actual === expected) {
+    console.log('passed');  
   } else {
-      console.log(`Failed ${testName}`);
+    console.log(`Failed: [${testName}] expected ${expected} but got ${actual}`);
   }
 }
-  
+
 // TESTS CASES
-function addOne(val) {
-  return val + 1;
-}
+let testObject = {
+  firstName: 'David',
+  lastName: 'Harbin'
+};
 
-let mapInput = [1, 2, 3, 4, 5];
-let actualMap = map(mapInput, addOne);
-let expectedMap = [2, 3, 4, 5, 6];
-assertArraysEqual(actualMap, expectedMap, 'should correctly map elements of an array');
 
-let cubeInput = [1, 2, 3, 4, 5];
-let actualCube = cubeAll(cubeInput);
-let exptectedCube = [1, 8, 27, 64, 125];
-assertArraysEqual(actualCube, exptectedCube, 'should correctly cube each element of an array');
+
+let actual = addFullNameProp(testObject);
+let expected = {
+  firstName: 'David',
+  lastName: 'Harbin',
+  fullName: 'David Harbin'
+};
+assertObjectsEqual(actual, expected, 'Should combine first and last name and assign to the fullName key');
