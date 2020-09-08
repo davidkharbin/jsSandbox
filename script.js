9a59c0c29df888f3d934cb98187b712e186ac3b6
davidkharbin <david_k_harbin@hotmail.com>
2020-09-07 21:14:44 -0600

    new dir for HR prep

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
+// Sample Input
+var classList = ["Joe", "Jack", "John", "Fred", "Frank", "Barry", "Larry", "Mary",
+"Harry", "Farrell", "Susan", "Monica", "Keira", "Caroline", "Harriet", "Erica",
+"Luann", "Cheryl", "Beth", "Rupa", "Linda", "Allison", "Nancy", "Dora"];
+
+// Sample Output
+var classListWithAges = [{"name":"Joe","age":11},{"name":"Jack","age":10},
+{"name":"John","age":11},{"name":"Fred","age":11},{"name":"Frank","age":11},
+{"name":"Barry","age":11},{"name":"Larry","age":11},{"name":"Mary","age":11},
+{"name":"Harry","age":11},{"name":"Farrell","age":10},{"name":"Susan","age":10},
+{"name":"Monica","age":11},{"name":"Keira","age":10},{"name":"Caroline","age":10},
+{"name":"Harriet","age":11},{"name":"Erica","age":11},{"name":"Luann","age":10},
+{"name":"Cheryl","age":11},{"name":"Beth","age":10},{"name":"Rupa","age":11},
+{"name":"Linda","age":10},{"name":"Allison","age":10},{"name":"Nancy","age":10},
+{"name":"Dora","age":10}];
+
+
 // FUNCTION DEFINITION(S)
-function addFullNameProp(obj) {
-  let firstName = obj['firstName'];
-  let lastName = obj['lastName'];
-  let fullName = `${firstName} ${lastName}`;
-  if (firstName && lastName) {
-    obj['fullName'] = fullName;
-  }
-  
-  return obj;
+
+// USE THIS FUNCTION TO GENERATE A RANDOM NUMBER
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function decorateClassListWithAges(classList) {
+  // creates an object for each string in the input array, with an age of 10 or 11
+  // returns an array of these objects
 }
 
 // ASSERTION FUNCTION(S) TO BE USED
-function assertObjectsEqual(actual, expected, testName) {
-  actual = JSON.stringify(actual);
-  expected = JSON.stringify(expected);
-
-  if (actual === expected) {
-    console.log('passed');  
-  } else {
-    console.log(`Failed: [${testName}] expected ${expected} but got ${actual}`);
-  }
-}
 
 // TESTS CASES
-let testObject = {
-  firstName: 'David',
-  lastName: 'Harbin'
-};
-
-
-
-let actual = addFullNameProp(testObject);
-let expected = {
-  firstName: 'David',
-  lastName: 'Harbin',
-  fullName: 'David Harbin'
-};
-assertObjectsEqual(actual, expected, 'Should combine first and last name and assign to the fullName key');
