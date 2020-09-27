8e72dde0d8799ec542dfaa4ceca3577989eca222
David Harbin <david_k_harbin@hotmail.com>
2020-09-27 08:34:52 -0600

    working on taa prep module 4

--- script.js
+++ script.js
@@ -1,66 +1,80 @@
 // Skeleton
 
 // FUNCTION DEFINITION(S)
-function average(numbers) {
-  // uses sum function
-  // returns the average of an array of numbers
-  let average = sum(numbers) / numbers.length;
-  return average;
+function findMaxRepeatCountInWord(word) {
+  // Break up individual word into individual letters.
+  let letters = word.split('');
+  let letterCount = {};
+
+  // Count the instances of each letter
+  for (let i = 0; i < letters.length; i++) {
+    let currentLetter = letters[i];
+    if (letterCount[currentLetter] === undefined) {
+      letterCount[currentLetter] = 1;
+    } else {
+      letterCount[currentLetter]++; 
+    }
+  };
+  
+  // Iterate all the counts and find the highest
+  let max = 0;
+  for (let letter in letterCount) {
+    if (letterCount[letter] > max) {
+      max = letterCount[letter];
+    }
+  };
+
+  
+  // Return this word's max repeat count
+  return max;
 }
 
-function sum(numbers) {
-  // returns the sum of an array of numbers
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-      sum += numbers[i];
-  }
-  return sum;
+function findFirstWordWithMostRepeatedChars(text) {
+  let maxRepeatCountOverall = 0;
+  let wordWithMaxRepeatCount = '';
+
+  // Break up input text into words (space-delimited).
+  text = text.split(' ');
+
+    for (let i = 0; i < text.length; i++) {
+      // For each word...
+      let repeatCountForWord = findMaxRepeatCountInWord(text[i]);
+      // If that max repeat count is higher than the overall max repeat count, then
+      if (repeatCountForWord > maxRepeatCountOverall) {
+        // update maxRepeatCountOverall
+        // update wordWithMaxRepeatCount
+        maxRepeatCountOverall = repeatCountForWord;
+        wordWithMaxRepeatCount = text[i];
+      }
+    }
+    
+  return wordWithMaxRepeatCount;
 }
 
 // ASSERTION FUNCTION(S) TO BE USED
-function assertEquals(actual, expected, testName) {
-    if (actual === expected) {
-        console.log('passed');
-    } else {
-        console.log(`Failed: Expected ${expected} but got ${actual} [${testName}]`);
-    }
+function assertEqual(actual, expected, testName) {
+  if (actual === expected) {
+    console.log('passed');
+  } else {
+    console.log(`FAILED: Expected ${expected}, but got ${actual}`);
+  }
 }
-
 
 // TESTS CASES
-let array_1 = [1, 2, 3, 4, 5];
-let expected_1 = 3;
-let actual_1 = average(array_1);
-let testName_1 = 'should equal 3';
-assertEquals(actual_1, expected_1, testName_1);
+let input_1 = 'One two three';
+let actual_1 = findFirstWordWithMostRepeatedChars(input_1);
+let expected_1 = 'three';
+let testName_1 = `Should return 'three'`;
+assertEqual(actual_1, expected_1, testName_1);
 
+let input_2 = 'Baseball football hockey';
+let actual_2 = findFirstWordWithMostRepeatedChars(input_2);
+let expected_2 = 'Baseball';
+let testName_2 = `Should return 'Baseball'`;
+assertEqual(actual_2, expected_2, testName_2);
 
-
-var output = sumDigits(-316);
-console.log(output); // --> 4
-
-var output = sumDigits(1148);
-console.log(output); // --> 14
-
-function sumDigits(num) {
-  let sum = 0;
-  let isNegative = false;
-  if (num < 0) {
-      isNegative = true;
-  }
-  
-  if (isNegative) {
-    let numArray = Array.from(num);
-    for (let i = 1; i < numArray.length; i++) {
-      sum += numArray[i];
-    }
-  } else {
-    let numArray = Array.from(num);
-    for (let i = 0; i < numArray.length; i++) {
-      sum += numArray[i];
-    }
-    
-  }
-
-  
-}
+let input_3 = 'Colorado Mississippi Alaska Montana';
+let actual_3 = findFirstWordWithMostRepeatedChars(input_3);
+let expected_3 = 'Mississippi';
+let testName_3 = `Should return 'Mississippi'`;
+assertEqual(actual_3, expected_3, testName_3);
