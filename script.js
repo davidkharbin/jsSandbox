e72a23690bc1be8b881ccf71d750bc90a65cc534
David Harbin <david_k_harbin@hotmail.com>
2020-09-29 13:08:04 -0600

    Style guide review

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
+// Skeleton
 
-
-let assigned = function(param) {
-  console.log(param);
+// FUNCTION DEFINITION(S)
+function findLongestPalindrome(sentence) {
+  let palindromesArray = [];  
+  
+  // split sentence into words
+  let wordArray = sentence.split(' ');
+  // iterate words and collect the palindromes
+  for (let i = 0; i < wordArray.length; i++) {
+      let currentWord = wordArray[i];
+      if (isPalindrome(currentWord)) {
+          palindromesArray.push(currentWord);
+      }
+      // sort the list of palindromes by word length
+      
+  }
+  
+  
+  // return the largest item in the sorted list
+  let longestPalindrome = palindromesArray[0];
+  for (let j = 0; j < palindromesArray.length; j++) {
+      if (palindromesArray[j].length >= longestPalindrome.length) {
+          longestPalindrome = palindromesArray[j];
+      }
+  }
+  return longestPalindrome;
 }
 
-let assignedName = function namedFunction(param) {
-  console.log(param);
+function reverseString(string) {
+  let reversedString = string.split("").reverse().join("");
+  return reversedString;
 }
 
-assigned('hello anonymous world');
-assignedName('hello named world');
+function isPalindrome(word) {
+  // hint: you can detect palindromes by comparing a string to its reverse
+  if (reverseString(word).toLowerCase() === word.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function sortAscendingByLength(a, b) {
+  if (a.length > b.length) {
+    return 1;
+  } else if (a.length < b.length) {
+    return -1;
+  }
+  return 0;
+}
+
+// ASSERTION FUNCTION(S) TO BE USED
+
+// TESTS CASES
