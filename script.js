5314e770a9b67add4345b50ed01c2ba920ca34d2
David K. Harbin <david_k_harbin@hotmail.com>
2020-12-22 12:02:43 -0700

    Working on debugging module today

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-reduce = function(collection, iterator, accumulator) {
-  if (accumulator === undefined) {
-    accumulator = collection[0];
+var fun = function (input) {
+  console.log(input.toUpperCase());
+}
+
+
+setTimeout(
+  function () {
+    fun('hi');
   }
-
-
-  for (var i = 0; i < collection.length; i++) {
-    if (iterator(accumulator, collection[i]) === undefined ) {
-      continue;
-    }
-    accumulator = iterator(accumulator, collection[i]);
-  }
-
-  return accumulator;
-
-};
-
-  // it('should pass the second item of the array into the iterator first if a memo is not passed in', function() {
-    var result = reduce([3, 2, 1], function(memo, item) {
-      console.log(item);
-      return memo - item;
-    });
-
-    // expect(result).to.equal(0);
-    console.log(result);
+  , 2000);
