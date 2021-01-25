f9b6669400feb53c0e3a75b6ca2b765ab9dd837e
David Harbin <dave@pop-os.localdomain>
2021-01-25 10:49:10 -0700

    practice

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-var fun = function (input) {
-  console.log(input.toUpperCase());
+var arr = [1, 1, 1, 1];
+
+
+
+function minMax(arr) {
+	var max = arr[0];
+	var min = arr[0];
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	return [min, max];
 }
 
-
-setTimeout(
-  function () {
-    fun('hi');
-  }
-  , 2000);
+console.log(minMax(arr));
