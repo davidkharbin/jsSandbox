f48c95b0a64820b7c83fab42cbb3ae0137d7b027
davidkharbin <david_k_harbin@hotmail.com>
2021-02-09 07:24:30 -0700

    Scopes and execution contexts

--- executionContexts.js
+++ executionContexts.js
@@ -32,20 +32,17 @@ var aFoil = function() {
 };
 
 
-
-
-
 var hero = aHero();
 
 var newSaga = function() {
   var foil = aFoil();
   var saga = function() {
-    var deed = aDeed();
-    console.log(`${hero} ${deed} ${foil}`);
   };
-  saga();
-  saga();
+  var deed = aDeed();
+  console.log(`${hero} ${deed} ${foil}`);
 };
+saga();
+saga();
 
 newSaga();
 newSaga();
