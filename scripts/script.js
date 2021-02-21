8a5238181c6c43ed8a6f3f57a57dfcbdc28bd4a0
davidkharbin <david_k_harbin@hotmail.com>
2021-02-21 06:19:48 -0700

    Codewars: decipherThis

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,38 @@
-function mxdiflg(a1, a2) {
-  if ((a1.length === 0 || a2.length === 0) || (a1.length === 0 && a2.length === 0)) {
-    return -1;
+
+console.log(decipherThis('72olle 103doo 100ya'));
+console.log(decipherThis('82yade 115te 103o'));
+; // 'Hello good day'
+; // 'Ready set go'
+
+
+function decipherThis(str) {
+  var words = str.split(' ');
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+    words[i] = decoded(word);
   }
+  return words.join(' ');
 
-  var longest = a1[0];
-  var longest2 = a2[0];
-  var shortest1 = a1[0];
-  var shortest2 = a2[0];
 
-  for (var i = 0; i < a1.length; i++) {
-    if (a1[i].length > longest1.length) {
-      longest1 = a1[i];
+  function decoded(word) {
+    var firstLetter = '';
+    var secondLetter;
+    var lastLetter;
+    var decodedWord;
+    var i = 0;
+    // Get the character code of the first letter
+    while (!isNaN(Number(word[i]))) {
+      firstLetter += word[i];
+      i++;
     }
-    if (a1[i].length < shortest1.length) {
-      shortest1 = a1[i];
-    }
-  }
+    word = word.substring(i, word.length).split('');
 
-  for (var j = 0; j < a2.length; j++) {
-    if (a2[j].length > longest2.length) {
-      longest2 = a2[j];
-    }
-    if (a2[j].length < shortest2.length) {
-      shortest2 = a2[j];
-    }
-  }
-
-  var maxDiffA = Math.abs(longest1.length - shortest2.length);
-  var maxDiffB = Math.abs(longest2.length - shortest1.length);
-
-
-  if (maxDiffA > maxDiffB) {
-    return maxDiffA;
-  }
-
-  return maxDiffB;
-
-}
-
-var s1 = [];
-    var s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"];
-    console.log(mxdiflg(s1, s2));
+    firstLetter = String.fromCharCode(firstLetter);
+    secondLetter = word[word.length - 1];
+    lastLetter = word[0];
+    word[0] = secondLetter;
+    word[word.length - 1] = lastLetter;
+    decodedWord = firstLetter + word.join('');
+    return  decodedWord;
+  };
+};
