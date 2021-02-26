6fe076cf6ed9290a5822b1f0219fdca9ddd6b2e0
davidkharbin <david_k_harbin@hotmail.com>
2021-02-25 21:10:25 -0700

    ready for recursion

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,6 @@
-// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  console.log( "ready!" );
 
-// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
-
-
-function isPangram(string){
-  var string = string.toLowerCase();
-  var pangram = true;
-  var asciis = [];
-
-  // get array of ascii codes
-  for (var i = 0; i < string.length; i++) {
-    var asciiCode = string.charCodeAt(i);
-    asciis.push(asciiCode);
-  }
-
-  // see if the ascii code (97 - 122)
-  // exist in the array of ascii codes
-  for (var j = 97; j <= 122; j++) {
-    if (!asciis.includes(j)) {
-      pangram = false;
-      break;
-    }
-  }
-  return pangram;
-};
-
-console.log(isPangram('The QUICK something a SOMETHING SOMETHING!!.'));
-
-console.log(isPangram('the quick brown fox jumps over the lazy dog'));
+  $('img').src = 'https://cdn1-www.dogtime.com/assets/uploads/2017/09/pit-bull-puppies-3-720x407.jpg';
+});
