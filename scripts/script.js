e7d8da892d209501e63048c54cf8673b63651c21
davidkharbin <david_k_harbin@hotmail.com>
2021-09-15 14:07:00 -0600

    DSA Masterclass:  areThereDuplicates solved

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,13 @@
-let testArr = [1, 3, 0, 3, 3, 44, 5, 1, 2, 3, 5, 6, 7, 7, 7, 8];
-let sortedArr = [-2, -1, -1, -1, 0, 1, 2, 3, 3, 3, 4, 9]; //8 ?
-
-const uniqueCount = function(arr) {
-  let i = 0;
-
-  for (let j = 1; j < arr.length; j++) {
-    if (arr[i] !== arr[j]){
-      i++;
-      arr[i] = arr[j];
-    }
+function averagePair(nums, k){
+  for (let i = 0; i < nums.length - 1; i++){
+      let j = i + 1;
+      while (j < nums.length || ((i + j) / 2) <= k){
+          if ( ((i + j) / 2) === k){
+              return true;
+          }
+          j++;
+      }
   }
-  console.log(arr)
-  return i+1;
-};
 
-console.log(uniqueCount(sortedArr))
+  return false;
+}
