7e0f6aae34fa01d98897d66637e13449b155b49d
davidkharbin <david_k_harbin@hotmail.com>
2021-09-15 14:11:02 -0600

    DSA Masterclass averagePair solved w/2 pointers

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
-function averagePair(nums, k){
-  for (let i = 0; i < nums.length - 1; i++){
-      let j = i + 1;
-      while (j < nums.length || ((i + j) / 2) <= k){
-          if ( ((i + j) / 2) === k){
-              return true;
-          }
-          j++;
-      }
-  }
+const averagePair = function(nums, k){
+  if ( nums.length === 0 ){return false;}
 
+  for (let i = 0; i < nums.length - 1; i++){
+    let j = i + 1;
+
+    while (j < nums.length || ((i + j) / 2) <= k){
+      if ( ((i + j) / 2) === k ){
+        return true;
+      }
+      j++;
+    }
+  }
   return false;
 }
