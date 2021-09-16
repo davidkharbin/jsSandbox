72507e6d07f9cb84907160f1c39ef0f267c6da85
davidkharbin <david_k_harbin@hotmail.com>
2021-09-16 15:33:10 -0600

    DSA Masterclass:  maxSubarraySum with pointers

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
-// does str2 contain str1 characters in order?
-const isSubsequence = function(str1, str2){
-  if ( str1.length > str2.length ){ return false; }
+const maxSubarraySum = function(nums, k) {
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+  let max = nums.slice(0, k).reduce(reducer);
+  let tempMax = max;
+  let j = k;
 
-  let i = 0;
-  let j = 0;
+  for ( let i = 0; j < nums.length; i++ ){
+    tempMax -= nums[i];
+    tempMax += nums[j];
 
-  while ( j < str2.length ){
-    if ( str1[i] === str2[j] ){
-      i++;
-    }
-
-    if ( i === str1.length ){
-      return true;
-    }
-
+    if (tempMax > max){max = tempMax}
     j++;
   }
+  console.log(max)
+  return max;
 };
+
+maxSubarraySum([100, 200, 300, 400], 2); //700
+maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4); //39
+maxSubarraySum([-3, 4, 0, -2, 6, -1], 2); //5
