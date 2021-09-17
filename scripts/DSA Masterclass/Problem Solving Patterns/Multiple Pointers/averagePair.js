811892016103684780336c3a9cc6e80160942f6c
davidkharbin <david_k_harbin@hotmail.com>
2021-09-16 19:08:12 -0600

    chore:  DSM cleanup

---
+++ scripts/DSA Masterclass/Problem Solving Patterns/Multiple Pointers/averagePair.js
@@ -0,0 +1,13 @@
+const averagePair = function(nums, k){
+	if ( nums.length === 0 ) return false;
+
+	for ( let i = 0; i < nums.length; i++ ){
+		let j = i + 1;
+
+		while ( j < nums.length || ((i + j) / 2) <= k ){
+			if ( ((i + j) / 2) === k ) return true;
+			j++;
+		}
+	}
+	return false;
+};
