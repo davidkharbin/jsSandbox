475481f34fb2461c07f306a57ab78c2cad4a1e61
davidkharbin <david_k_harbin@hotmail.com>
2021-09-20 20:40:33 -0600

    DSA Masterclass:  beginning recursion section

--- scripts/DSA Masterclass/Problem Solving Patterns/Sliding Window/findLongestSubstring.js
+++ scripts/DSA Masterclass/Problem Solving Patterns/Sliding Window/findLongestSubstring.js
@@ -1,21 +1,29 @@
-const findLongestSubstring = function(str) {
-	let longest = 0;
-  let seenLetters = {};
-  let start = 0;
+// const findLongestSubstring = function(str) {
+// 	let longest = 0;
+//   let seenLetters = {};
+//   let start = 0;
 
-  for (let i = 0; i < str.length; i++) {
-		if ( (str.length - start) < longest) break;
+//   for (let i = 0; i < str.length; i++) {
+// 		if ( (str.length - start) < longest) break;
 
-    let letter = str[i];
-    if (seenLetters[letter]) {
-      start = Math.max(start, seenLetters[letter]);
-    }
+//     let letter = str[i];
+//     if (seenLetters[letter]) {
+//       start = Math.max(start, seenLetters[letter]);
+//     }
 
-    longest = Math.max(longest, i - start + 1);
+//     longest = Math.max(longest, i - start + 1);
 
-    seenLetters[letter] = i + 1;
-  }
-  return longest;
-};
+//     seenLetters[letter] = i + 1;
+//   }
+//   return longest;
+// };
 
-console.log(findLongestSubstring('thecatinthehat'));
+// console.log(findLongestSubstring('thecatinthehat'));
+
+function productOfArray(arr) {
+	if ( arr.length === 0) return 0;
+
+	return arr[0] + productOfArray(arr.slice(1))
+}
+console.log(productOfArray([1, 2, -3, 4, 5, 6]))
+ // 21
