2bd0c5c9cb6d5f7deb4dbe5d449d42e22230a73d
davidkharbin <david_k_harbin@hotmail.com>
2021-09-23 21:22:07 -0600

    DSA Masterclass:  solved binarySearch w/ multiple pointers

--- scripts/DSA Masterclass/Searching Algorithms/binarySearch.js
+++ scripts/DSA Masterclass/Searching Algorithms/binarySearch.js
@@ -1,6 +1,33 @@
-const binarySearch = function(){
+const binarySearch = function(arr, target){
+  let left = 0;
+	let right = arr.length - 1;
 
+	while ( target > arr[left] && target < arr[right] ){
+		let val = Math.floor(( right + left ) / 2);
+
+		if ( arr[val] === target ){
+			return val;
+		} else if ( arr[val] < target ){
+			left = val + 1;
+		} else {
+			right = val - 1;
+		}
+	}
+
+	if ( arr[left]  === target ) return left;
+	if ( arr[right] === target ) return right;
+	return -1;
 };
+
+
+console.log('1', binarySearch([1, 2, 3, 4, 5], 2))  // 1
+console.log('2', binarySearch([1, 2, 3, 4, 5], 3))  // 2
+console.log('3', binarySearch([1, 2, 3, 4, 5], 5))	 // 4
+console.log('4', binarySearch([1, 2, 3, 4, 5], 6))  // -1
+console.log('5', binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 10))  //  2
+console.log('6', binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 95))  //  16
+console.log('7', binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 100))  //  -1
+
 
 /**
  * binary search must be sorted
