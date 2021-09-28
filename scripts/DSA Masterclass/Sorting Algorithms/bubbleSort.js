df508e3db4e5207218ea837b525945bfa2a9b7fa
davidkharbin <david_k_harbin@hotmail.com>
2021-09-28 13:47:05 -0600

    DSA Masterclass:  optimized bubbleSort

--- scripts/DSA Masterclass/Sorting Algorithms/bubbleSort.js
+++ scripts/DSA Masterclass/Sorting Algorithms/bubbleSort.js
@@ -1,36 +1,17 @@
 const bubbleSort = function(arr) {
 
-	// let wasChanged = false;
-
-	// for (let i = 0; i < arr.length - 1; i++) {
-	// 	let currentValue = arr[i];
-	// 	let nextValue = arr[i + 1];
-	// 	console.log('iteration')
-	// 	if (currentValue > nextValue) {
-	// 		arr[i] = nextValue;
-	// 		arr[i + 1] = currentValue;
-	// 		wasChanged = true;
-	// 	}
-
-	// }
-
-	// if (wasChanged) {
-	// 	bubbleSort(arr)
-	// }
-	// return arr;
-
-	let noSwaps;
+	let unchanged;
   for(var i = arr.length; i > 0; i--){
-    noSwaps = true;
+    unchanged = true;
     for(let j = 0; j < i - 1; j++){
       if(arr[j] > arr[j+1]){
         let temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
-        noSwaps = false;
+        unchanged = false;
       }
     }
-    if(noSwaps) break;
+    if(unchanged) break;
   }
   return arr;
 };
