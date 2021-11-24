86e46a00a1ffd8ece8cca170583f69f4b854396b
davidkharbin <david_k_harbin@hotmail.com>
2021-11-24 10:11:22 -0700

    docs: adds notes on quickSort

---
+++ scripts/DSA Masterclass/Sorting Algorithms/quickSort.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Like merge sort, use the fact that arrays of length 0 or 1 are already sorted
+ * 
+ * Selects one element, called the pivot, and finding the index where the pivot should end up in the sorted array
+ * 
+ * Once pivot is positioned appropriately, quickSort can be applied on either side of the pivot
+ */
