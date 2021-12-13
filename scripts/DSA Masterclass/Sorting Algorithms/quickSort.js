21271461c43f1ed37a676718bc4e5a9cbb7b7231
davidkharbin <david_k_harbin@hotmail.com>
2021-12-13 09:43:17 -0700

    build: quickSort complete

--- scripts/DSA Masterclass/Sorting Algorithms/quickSort.js
+++ scripts/DSA Masterclass/Sorting Algorithms/quickSort.js
@@ -1,37 +1,35 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // pivot helper
-const pivot = (arr, beginIdx = 0, endIdx = arr.length - 1) => {
+const pivot = (arr, start = 0, end = arr.length - 1) => {
 	const swap = (array, idx1, idx2) => {
 		[array[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
 	}
 
-	let pivotVal = arr[beginIdx];
-	let swapIdx = beginIdx;
+	let pivotVal = arr[start];
+	let swapIdx = start;
 
-	for (let i = beginIdx + 1; i < endIdx; i++) {
-		if (pivotVal > arr[i]){
+	for (let i = start + 1; i <= end; i++) {
+		if (pivotVal > arr[i]) {
 			swapIdx++;
 			swap(arr, swapIdx, i);
 		}
 	}
+	swap(arr, start, swapIdx);
+	return swapIdx;
 }
 
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+	if (left < right) {
+		let pivotIndex = pivot(arr, left, right);
+		// left
+		quickSort(arr, left, pivotIndex - 1);
+		// right
+		quickSort(arr, pivotIndex + 1, right);
+	}
+	return arr;
+};
+
 let arr = [4, 8, 2, 1, 5, 7, 6, 3];
-pivot(arr)
-console.log(arr)
+console.log(quickSort(arr))
 
 
 
