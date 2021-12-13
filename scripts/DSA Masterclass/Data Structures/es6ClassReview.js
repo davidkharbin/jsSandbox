035178436a6aca3cdecd362f5ce265e5e7dc94d0
davidkharbin <david_k_harbin@hotmail.com>
2021-12-13 11:29:39 -0700

    Ignore unwanted files

---
+++ scripts/DSA Masterclass/Data Structures/es6ClassReview.js
@@ -0,0 +1,37 @@
+class Student {
+	constructor(firstName, lastName, year){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.year = year;
+	}
+
+}
+
+
+
+
+let david = new Student(david, harbin, 4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Class: "A blueprint for creating objects with pre-defined properties and methods"
+ * 
+ * In javascript, this is not TRUE OOP, it's syntactical sugar for it's prototypal inheritance system
+ */
