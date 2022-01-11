902698ab96322968489fbdaf7dace0924bdfa2a8
davidkharbin <david_k_harbin@hotmail.com>
2022-01-11 13:53:56 -0700

    build: completed push for DoublyLinkedList

---
+++ scripts/DSA Masterclass/Data Structures/Doubly_Linked_Lists.js
@@ -0,0 +1,38 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+		this.prev = null;
+	}
+};
+
+class DoublyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	push(value) {
+		let newNode = new Node(value);
+		if (this.length === 0) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			let temp = this.tail;
+			this.tail.next = newNode;
+			newNode.prev = this.tail;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
+}
+var dll = new DoublyLinkedList();
+dll.push(1)
+dll.push(2)
+dll.push(3)
+dll.push(4)
+
+
+module.exports = { Node, DoublyLinkedList }
