a64c999d341dd1ed78700ae5bbab9a7f1dcc711f
davidkharbin <david_k_harbin@hotmail.com>
2022-01-28 16:20:05 -0700

    build(DSA): queue classes added

--- scripts/DSA Masterclass/Data Structures/Queue.js
+++ scripts/DSA Masterclass/Data Structures/Queue.js
@@ -1,1 +1,14 @@
-// queue
+class Node {
+	constructor(value){
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Queue {
+	constructor(){
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
+}
