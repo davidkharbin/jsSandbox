01b08d7d35852a29afcb0ca53484a5537ad7737e
davidkharbin <david_k_harbin@hotmail.com>
2022-01-28 20:29:47 -0700

    build(DSA): queue methods complete

--- scripts/DSA Masterclass/Data Structures/Queue.js
+++ scripts/DSA Masterclass/Data Structures/Queue.js
@@ -11,4 +11,44 @@ class Queue {
 		this.last = null;
 		this.size = 0;
 	}
+
+	enqueue(value) {
+		let newNode = new Node(value);
+
+		if (this.size === 0){
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			this.last.next = newNode;
+			this.last = newNode;
+		}
+
+		this.size++;
+		return this;
+	}
+
+	dequeue(){
+		if (this.size === 0) return null;
+		
+	  let cache = this.first;
+		if (this.size === 1){
+			this.first = null;
+			this.last = null;
+			this.size--;
+			return cache;
+		} else {
+			this.first = this.first.next;
+			cache.next = null;
+			this.size--;
+			return cache;
+		}
+	}
 }
+
+var q = new Queue();
+
+q.enqueue(0);
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+q.enqueue(4);
