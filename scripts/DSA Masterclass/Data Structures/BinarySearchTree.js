556d69d1a9b00e881c1ba3e229245a52a915eca8
davidkharbin <david_k_harbin@hotmail.com>
2022-02-02 17:05:17 -0700

    build(DSA): insert method for BST added

--- scripts/DSA Masterclass/Data Structures/BinarySearchTree.js
+++ scripts/DSA Masterclass/Data Structures/BinarySearchTree.js
@@ -1,17 +1,60 @@
 class Node {
-	constructor(value){
+	constructor(value) {
+		this.count = 1;
+		this.left = null;
+		this.right = null;
 		this.value = value;
 	}
 }
 
 class BinarySearchTree {
-	constructor(){
+	constructor() {
 		this.root = null;
 	}
+
+	insert(value) {
+		let newNode = new Node(value);
+		if (!this.root) {
+			this.root = newNode;
+			return this;
+		}
+
+		let currentNode = this.root;
+		while (true) {
+			let goLeft = newNode.value < currentNode.value;
+			let goRight = newNode.value > currentNode.value;
+			let areEqual = newNode.value === currentNode.value;
+
+			if (goLeft) {
+				if (!currentNode.left) {
+					currentNode.left = newNode;
+					return this;
+				} else {
+					currentNode = currentNode.left;
+				}
+			}
+			if (goRight) {
+				if (!currentNode.right) {
+					currentNode.right = newNode;
+					return this;
+				} else {
+					currentNode = currentNode.right;
+				}
+			}
+			if (areEqual){
+				currentNode.count++;
+				return 'duplicate added';
+			}
+		}
+	}
+
 }
 
 var tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
