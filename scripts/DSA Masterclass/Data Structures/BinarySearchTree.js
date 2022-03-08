303fb0e0691ce364aa8e4e29ba5b0aaabf8faadd
davidkharbin <david_k_harbin@hotmail.com>
2022-03-08 11:20:38 -0700

    build: added proper Queuek to Binary Search

--- scripts/DSA Masterclass/Data Structures/BinarySearchTree.js
+++ scripts/DSA Masterclass/Data Structures/BinarySearchTree.js
@@ -1,3 +1,49 @@
+class QueueNode {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Queue {
+	constructor(){
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
+	
+	enqueue(value) {
+		let newNode = new QueueNode(value);
+		
+		if (this.size === 0){
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			this.last.next = newNode;
+			this.last = newNode;
+		}
+		this.size++;
+		return newNode;
+	}
+	
+	dequeue(){
+		if (this.size === 0) return null;
+		
+	  let cache = this.first;
+		if (this.size === 1){
+			this.first = null;
+			this.last = null;
+			this.size--;
+			return cache;
+		} else {
+			this.first = this.first.next;
+			cache.next = null;
+			this.size--;
+			return cache;
+		}
+	}
+}
+
 class TreeNode {
 	constructor(value) {
 		this.count = 1;
@@ -92,6 +138,7 @@ class BinarySearchTree {
 		let data = [];
 		let queue = new Queue();
 		queue.enqueue(node);
+		
 		while (queue.size > 0) {
 			let val = queue.first.value.value;
 			data.push(val);
